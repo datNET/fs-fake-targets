49b5e4e3a0199709f257dd91fe7387c496519906
Mathew Glodack <glodack@gmail.com>
2016-04-23 12:06:34 -0700

    Update config parameters
    * Create specific nuget paramater types on
    base config params record type

--- src/FSharpFakeTargets/targets.fs
+++ src/FSharpFakeTargets/targets.fs
@@ -17,7 +17,14 @@ module Targets =
       MSBuildReleaseArtifacts : FileIncludes
       MSBuildOutputDir : string
       NuspecFilePath : Option<string>
-      NuGetParams : NuGetParams
+      Version : string
+      Project : string
+      Authors : string list
+      Description : string
+      OutputPath : string
+      WorkingDir : string
+      Publish : bool
+      AccessKey : string
     }
 
   let ConfigDefaults() =
@@ -27,7 +34,14 @@ module Targets =
       MSBuildReleaseArtifacts = !! "**/bin/Release/*"
       MSBuildOutputDir = "bin"
       NuspecFilePath = TryFindFirstMatchingFile "*.nuspec" "."
-      NuGetParams = NuGetDefaults()
+      Version = String.Empty
+      Project = String.Empty
+      Authors = List.Empty
+      Description = String.Empty
+      OutputPath = String.Empty
+      WorkingDir = String.Empty
+      Publish = false
+      AccessKey = String.Empty
     }
 
   let private _EnsureNuspecFileExists filePath =
@@ -38,6 +52,19 @@ module Targets =
   let private _CreateTarget targetName parameters targetFunc =
     Target targetName targetFunc
     parameters
+
+  let private _CreateNuGetParams parameters =
+    (fun (nugetParams : NuGetParams) ->
+        { nugetParams with
+            Version = parameters.Version
+            Project = parameters.Project
+            Authors = parameters.Authors
+            Description = parameters.Description
+            OutputPath = parameters.OutputPath
+            WorkingDir = parameters.WorkingDir
+            Publish = parameters.Publish
+            AccessKey = parameters.AccessKey
+        })
 
   let private _MSBuildTarget parameters =
     _CreateTarget "MSBuild" parameters (fun _ ->
@@ -58,12 +85,12 @@ module Targets =
     _CreateTarget "Package" parameters (fun _ ->
         parameters.NuspecFilePath
             |> _EnsureNuspecFileExists
-            |> NuGetPack (fun nugetParams -> parameters.NuGetParams)
+            |> NuGetPack (_CreateNuGetParams parameters)
     )
 
   let private _PublishTarget parameters =
     _CreateTarget "Publish" parameters (fun _ ->
-        NuGetPublish (fun nugetParams -> parameters.NuGetParams)
+        NuGetPublish (_CreateNuGetParams parameters)
     )
 
   let Initialize setParams =
