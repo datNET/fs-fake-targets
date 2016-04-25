eb52bd9002b95a11aa0c40077fa1e29a7c4019ad
Andrew William Seward <awswrd@gmail.com>
2016-04-24 17:21:01 -0700

    Increment patch -> 0.4.2

--- AssemblyInfo.fs
+++ AssemblyInfo.fs
@@ -1,4 +1,4 @@
-﻿namespace FSharpFakeTargets.AssemblyInfo
+namespace FSharpFakeTargets.AssemblyInfo
 
 open System.Resources
 open System.Reflection
@@ -33,9 +33,9 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("0.4.1.0")>]
-[<assembly: AssemblyFileVersion("0.4.1.0")>]
-[<assembly: AssemblyInformationalVersion("0.4.1")>]
+[<assembly: AssemblyVersion("0.4.2.0")>]
+[<assembly: AssemblyFileVersion("0.4.2.0")>]
+[<assembly: AssemblyInformationalVersion("0.4.2")>]
 
 do
     ()
