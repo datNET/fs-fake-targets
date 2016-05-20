56e982df9bd61f3717a8380b5b54f93f80977bbd
Andrew William Seward <awswrd@gmail.com>
2016-05-20 00:16:23 -0700

    Add a note about self-testing locally

--- build.fsx
+++ build.fsx
@@ -1,5 +1,18 @@
 #r @"./._fake/packages/FAKE/tools/FakeLib.dll"
+
 #r @"./._fake/packages/FSharp.FakeTargets/tools/FSharpFakeTargets.dll"
+(*
+  The commented line below is for local testing purposes. Make sure to
+
+  1. Comment out the line above
+  2. Uncommeht the line below
+  2. Build this solution in VS
+
+  After having done this, you should be able to try out whatever work you have
+  done on the source code for FSharpFakeTargets without having to release or
+  anything like that.
+*)
+//#r @"./src/FSharpFakeTargets/bin/Debug/FSharpFakeTargets.dll"
 
 #load @"./._fake/loader.fsx"
 
