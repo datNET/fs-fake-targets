95c935b3e3aeca44370c68f2642f4a5a36513513
Andrew William Seward <awswrd@gmail.com>
2016-05-21 13:09:45 -0700

    Consolidate loading/referencing to loader

--- ._fake/loader.fsx
+++ ._fake/loader.fsx
@@ -1,1 +1,16 @@
-#load @"./config.fsx"
+#r    @"packages/FAKE/tools/FakeLib.dll"
+#r    @"packages/FSharp.FakeTargets/tools/FSharpFakeTargets.dll"
+(*
+  The commented line below is for local testing purposes. Make sure to
+
+  1. Comment out the line above
+  2. Uncommeht the line below
+  2. Build this solution in VS
+
+  After having done this, you should be able to try out whatever work you have
+  done on the source code for FSharpFakeTargets without having to release or
+  anything like that.
+*)
+// #r @"../src/FSharpFakeTargets/bin/Debug/FSharpFakeTargets.dll"
+
+#load @"config.fsx"
