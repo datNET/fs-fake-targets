aaeddd5e615ce8413db2738e175d871ef3d73369
Andrew William Seward <awswrd@gmail.com>
2016-05-21 17:39:56 -0700

    Increment patch -> 0.8.1

--- AssemblyInfo.fs
+++ AssemblyInfo.fs
@@ -35,7 +35,7 @@ open System.Runtime.InteropServices
 // [<assembly: AssemblyVersion("1.0.*")>]
 [<assembly: AssemblyVersion("0.8.1.0")>]
 [<assembly: AssemblyFileVersion("0.8.1.0")>]
-[<assembly: AssemblyInformationalVersion("0.8.1-alpha")>]
+[<assembly: AssemblyInformationalVersion("0.8.1")>]
 
 do
     ()
