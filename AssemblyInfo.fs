44dfd4c3c0d5809c38c2578dc7e5b213f786d7b9
Drew Seward <awswrd@gmail.com>
2017-10-07 15:52:14 -0700

    version: Pre-increment patch -> 0.11.3-b

--- AssemblyInfo.fs
+++ AssemblyInfo.fs
@@ -35,7 +35,7 @@ open System.Runtime.InteropServices
 // [<assembly: AssemblyVersion("1.0.*")>]
 [<assembly: AssemblyVersion("0.11.3.0")>]
 [<assembly: AssemblyFileVersion("0.11.3.0")>]
-[<assembly: AssemblyInformationalVersion("0.11.3-a")>]
+[<assembly: AssemblyInformationalVersion("0.11.3-b")>]
 
 do
     ()
